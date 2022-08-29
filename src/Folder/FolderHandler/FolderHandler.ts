a0bd4f17234d0faf2a015f64851bb6ce0b9caf9b
amaurycoudr <amaury.coudray@gmail.com>
2022-08-29 17:28:22 +0200

    feat(folderhandler): creat class handle dir creation + add file

--- src/Folder/FolderHandler/FolderHandler.ts
+++ src/Folder/FolderHandler/FolderHandler.ts
@@ -1,7 +1,27 @@
+import FileWriter from '../../FileWriter/FileWriter';
+import FolderCreator from '../FolderCreator/FolderCreator';
+
 export default class FolderHandler {
   constructor(
     private path: string,
     private folderContent: Record<string, string>
   ) {}
-  
+
+  public setUpFolder = () => {
+    this.createFolder();
+    this.createFolderContent();
+  };
+
+  private createFolder = () => {
+    new FolderCreator(this.path).createFolder();
+  };
+
+  private createFolderContent = () => {
+    Object.entries(this.folderContent).forEach(([path, content]) => {
+      new FileWriter(path, content).write({
+        silent: true,
+        lintAfterWriting: true,
+      });
+    });
+  };
 }
